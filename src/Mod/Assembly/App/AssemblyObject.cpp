6c6b818bc8bf051ab35116d48b6ff3feac416ad9
Chris Hennes <chennes@gmail.com>
2026-03-17 11:08:26 -0500

    Assembly: Compiler warning cleanup
    Warnings from clang 18

--- src/Mod/Assembly/App/AssemblyObject.cpp
+++ src/Mod/Assembly/App/AssemblyObject.cpp
@@ -717,7 +717,7 @@ ViewGroup* AssemblyObject::getExplodedViewGroup() const
     return nullptr;
 }
 
-std::vector<App::DocumentObject*> AssemblyObject::getJoints(bool updateJCS, bool delBadJoints, bool subJoints)
+std::vector<App::DocumentObject*> AssemblyObject::getJoints([[maybe_unused]] bool updateJCS, bool delBadJoints, bool subJoints)
 {
     std::vector<App::DocumentObject*> joints = {};
 
